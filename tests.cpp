0fd9faa9f534d549ea7ace494bb5386ae8567619
A Student <student@nowhere.edu>
2025-11-16 06:43:27 +0000

    Nov 15, 2025, 10:43 PM

--- tests.cpp
+++ tests.cpp
@@ -67,38 +67,38 @@ string reformatTime(const string& time24)
 //-----------------------------------------------------------------------------------
 // Testing code
 
-// TEST_CASE("formatTimeToAMPM")
-// {
-//   cout << "1: formatTimeToAMPM" << endl;
-//   CHECK(formatTimeToAMPM(17, 54) == "5:54 PM");
-//   CHECK(formatTimeToAMPM(12, 30) == "12:30 PM");
-//   CHECK(formatTimeToAMPM(3, 5) == "3:05 AM");
-//   CHECK(formatTimeToAMPM(0, 0) == "12:00 AM");
-// }
+ TEST_CASE("formatTimeToAMPM")
+ {
+   cout << "1: formatTimeToAMPM" << endl;
+   CHECK(formatTimeToAMPM(17, 54) == "5:54 PM");
+   CHECK(formatTimeToAMPM(12, 30) == "12:30 PM");
+   CHECK(formatTimeToAMPM(3, 5) == "3:05 AM");
+   CHECK(formatTimeToAMPM(0, 0) == "12:00 AM");
+ }
 
-// TEST_CASE("formatTimeToAMPM exception")
-// {
-//   cout << "2: formatTimeToAMPM exception handling" << endl;
-//   CHECK_THROWS(formatTimeToAMPM(24, 0));
-//   CHECK_THROWS(formatTimeToAMPM(12, 60));
-//   CHECK_THROWS(formatTimeToAMPM(-1, 0));
-//   CHECK_THROWS(formatTimeToAMPM(0, -1));
-// }
+ TEST_CASE("formatTimeToAMPM exception")
+ {
+   cout << "2: formatTimeToAMPM exception handling" << endl;
+   CHECK_THROWS(formatTimeToAMPM(24, 0));
+   CHECK_THROWS(formatTimeToAMPM(12, 60));
+   CHECK_THROWS(formatTimeToAMPM(-1, 0));
+   CHECK_THROWS(formatTimeToAMPM(0, -1));
+ }
 
-// TEST_CASE("reformatTime")
-// {
-//   cout << "3: reformatTime" << endl;
-//   CHECK(reformatTime("17:54:48") == "5:54 PM");
-//   CHECK(reformatTime("12:30:15") == "12:30 PM");
-//   CHECK(reformatTime("03:05:09") == "3:05 AM");
-//   CHECK(reformatTime("00:00:00") == "12:00 AM");
-// }
+ TEST_CASE("reformatTime")
+ {
+   cout << "3: reformatTime" << endl;
+   CHECK(reformatTime("17:54:48") == "5:54 PM");
+   CHECK(reformatTime("12:30:15") == "12:30 PM");
+   CHECK(reformatTime("03:05:09") == "3:05 AM");
+   CHECK(reformatTime("00:00:00") == "12:00 AM");
+ }
 
-// TEST_CASE("reformatTime bad times")
-// {
-//   cout << "4: reformatTime exception handling" << endl;
-//   CHECK(reformatTime("11:60:12") == "???"); // minutes invalid
-//   CHECK(reformatTime("11:99:12") == "???"); // minutes invalid
-//   CHECK(reformatTime("25:11:12") == "???"); // hours invalid
-//   CHECK(reformatTime("99:11:12") == "???"); // hours invalid
-// }
+ TEST_CASE("reformatTime bad times")
+ {
+   cout << "4: reformatTime exception handling" << endl;
+   CHECK(reformatTime("11:60:12") == "???"); // minutes invalid
+   CHECK(reformatTime("11:99:12") == "???"); // minutes invalid
+   CHECK(reformatTime("25:11:12") == "???"); // hours invalid
+   CHECK(reformatTime("99:11:12") == "???"); // hours invalid
+ }
